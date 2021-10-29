504cb3cef052238b1235a13fc620134e83971f0a
Steve <steve.ellis@colorado.edu>
2021-10-29 16:57:24 -0400

    Adding files
    This adds the files from James' previous work which was based on the pulumi examples
    aws eks cluster. There are a few things I want to do:
    * Consider adding more tags
    * Making the stack (dev, prod, etc) be part of what gets tagged
    
    I also want to better understand resource naming and mitigate any potential collisions
    with existing resources before running.

--- pulumi/folio/__main__.py
+++ pulumi/folio/__main__.py
@@ -1,10 +1,65 @@
 """An AWS Python Pulumi program"""
 
+import iam
+import vpc
+import utils
 import pulumi
-from pulumi_aws import s3
+import pulumi_random
+import pulumi_kubernetes as k8s
+from pulumi_aws import eks, s3
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('steves-test-bucket')
+## EKS Cluster
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+#####
+# I am using `pulumi.ResourceOptions(parent)` a lot to control flow order and make sure that a
+# resource waits until the underlying dependencies are ready.
+#####
+
+eks_cluster = eks.Cluster(
+    "folio-eks-cluster",
+    role_arn=iam.eks_role.arn,
+    tags={
+        "Name": "folio-eks-cluster",
+        "Service": "FOLIO",
+        "Environment": "dev",
+        "Owner": "CTA",
+        "Product": "FOLIO",
+        "Accounting": "cubl-folio",
+        "DataClassification/Compliance": "standard",
+    },
+    vpc_config=eks.ClusterVpcConfigArgs(
+        # See also [Cluster VPC considerations](https://docs.aws.amazon.com/eks/latest/userguide/network_reqs.html)
+        public_access_cidrs=["0.0.0.0/0"],
+        # TODO See also [Amazon EKS security group considerations](https://docs.aws.amazon.com/eks/latest/userguide/sec-group-reqs.html)
+        security_group_ids=[vpc.eks_security_group.id],
+        # Creates a subnet and ec2 routeTableAssociate in each AZ in the region
+        subnet_ids=vpc.subnet_ids,
+    ),
+    version="1.21",
+)
+
+eks_node_group = eks.NodeGroup(
+    "folio-eks-nodegroup",
+    cluster_name=eks_cluster.name,
+    node_group_name="folio-eks-nodegroup",
+    node_role_arn=iam.ec2_role.arn,
+    subnet_ids=vpc.subnet_ids,
+    tags={
+        "Name": "folio-eks-nodes",
+        "Service": "FOLIO",
+        "Environment": "dev",
+        "Owner": "CTA",
+        "Product": "FOLIO",
+        "Accounting": "cubl-folio",
+        "DataClassification/Compliance": "standard",
+    },
+    instance_types=["t3.large"],
+    scaling_config=eks.NodeGroupScalingConfigArgs(
+        desired_size=3,
+        max_size=4,
+        min_size=1,
+    ),
+)
+
+pulumi.export("cluster-name", eks_cluster.name)
+pulumi.export("kubeconfig", utils.generate_kube_config(eks_cluster))
