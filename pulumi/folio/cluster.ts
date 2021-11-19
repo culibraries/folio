73696839c16ae6d7c04f7ab2588480bd45ff35f2
Steve <steve.ellis@colorado.edu>
2021-11-19 17:27:43 -0500

    This should resolve RBAC
    RBAC is implemented, tested and documented. Also broke things out into their own
    modules.

---
+++ pulumi/folio/cluster.ts
@@ -0,0 +1,96 @@
+import * as eks from "@pulumi/eks";
+import * as awsx from "@pulumi/awsx";
+import * as aws from "@pulumi/aws";
+import * as k8s from "@pulumi/kubernetes";
+
+export module deploy {
+    export function awsEksCluster(
+        name: string,
+        tags: object,
+        vpc: awsx.ec2.Vpc,
+        sg: aws.ec2.SecurityGroup,
+        workerRole: aws.iam.Role,
+        adminRole: aws.iam.Role): eks.Cluster {
+        const cluster = new eks.Cluster(name, {
+            // Applies tags to all resources under management of the cluster.
+            tags: { "Name": name, ...tags },
+
+            // Apply tags to this cluster.
+            clusterTags: { "Name": name, ...tags },
+
+            // Tell the cluster about its VPC.
+            vpcId: vpc.id,
+            publicSubnetIds: vpc.publicSubnetIds,
+            privateSubnetIds: vpc.privateSubnetIds,
+            nodeAssociatePublicIpAddress: false,
+
+            // Need this because we're going to create our own NodeGroup below.
+            skipDefaultNodeGroup: true,
+
+            // Bind the SecurityGroup to the cluster. If we don't do this it will create
+            // its own default one.
+            clusterSecurityGroup: sg,
+
+            // Bind any roles we have created to the cluster.
+            instanceRoles: [workerRole],
+
+            // Enable control plane logging. This sends logs to CloudWatch.
+            enabledClusterLogTypes: [
+                "api",
+                "audit",
+                "authenticator"
+            ],
+
+            // Set the desired kubernetes version.
+            version: "1.21",
+
+            roleMappings: [
+                // Provides full administrator cluster access to the k8s cluster.
+                {
+                    groups: ["system:masters"],
+                    roleArn: adminRole.arn,
+                    username: "pulumi:admin-usr",
+                }
+            ],
+        });
+
+        return cluster;
+    }
+
+    export function awsCreateEksNodeGroup(nodeGroupArgs: any, cluster: eks.Cluster, ip: aws.iam.InstanceProfile) {
+        cluster.createNodeGroup(nodeGroupArgs.name, {
+            instanceType: aws.ec2.InstanceType.T3_XLarge,
+            desiredCapacity: nodeGroupArgs.desiredCapacity,
+            minSize: nodeGroupArgs.minSize,
+            maxSize: nodeGroupArgs.maxSize,
+
+            // On-demand are the more expensive, non-spot instances.
+            labels: nodeGroupArgs.labels,
+
+            // Bind the instance profile to the NodeGroup.
+            instanceProfile: ip
+        });
+    }
+
+    export function awsApplyClusterAdminRoleToCluster(adminRoleName: string, cluster: eks.Cluster) {
+        new k8s.rbac.v1.ClusterRole(adminRoleName, {
+            metadata: {
+                name: adminRoleName
+            },
+            rules: [{
+                apiGroups: ["*"],
+                resources: ["*"],
+                verbs: ["*"],
+            }]
+        }, { provider: cluster.provider });
+    }
+
+    export function awsAddOn(name: string, addOnName:string, tags: object, cluster: eks.Cluster) {
+        const vpcCniName = "folio-vpc-cni-addon"
+        new aws.eks.Addon(name, {
+            tags: { "Name": name, ...tags },
+            clusterName: cluster.eksCluster.name,
+            addonName: "vpc-cni",
+        });
+    }
+}
---
+++ pulumi/folio/vpc.ts
@@ -0,0 +1,91 @@
+import * as awsx from "@pulumi/awsx";
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import { Output } from "@pulumi/pulumi";
+
+export module deploy {
+    export function awsVpc(
+        name: string, tags: object, availabilityZones: number, natGateways: number): awsx.ec2.Vpc {
+        const vpc = new awsx.ec2.Vpc(name, {
+            tags: { "Name": name, ...tags },
+
+            // If we have too many AZs and NAT gateways we'll run out of EIPs with our
+            // current quota. 
+            numberOfAvailabilityZones: availabilityZones,
+
+            // We could have these be equal to the number of availability zones for greater
+            // fault tolerance. Although the cost will be higher.
+            numberOfNatGateways: natGateways,
+
+            // See https://github.com/pulumi/pulumi-eks/blob/e03751425604b5193c00cd2f8858d9b16f7901f0/examples/subnet-tags/index.ts
+            // for what's going on with the tags here.
+            subnets: [{
+                type: "public", name: "folio-subnet",
+                tags: { "kubernetes.io/role/elb": "1", ...tags }
+            },
+            {
+                type: "private", name: "folio-subnet",
+                tags: { "kubernetes.io/role/internal-elb": "1", ...tags }
+            }]
+        },
+            {
+                // Inform pulumi to ignore tag changes to the VPCs or subnets, so that
+                // tags auto-added by AWS EKS do not get removed during future
+                // refreshes and updates, as they are added outside of pulumi's management
+                // and would be removed otherwise.
+                // Also see: https://github.com/pulumi/pulumi-eks/blob/master/examples/subnet-tags/index.ts
+                // for links to issues with the background on this.
+                transformations: [(args: any) => {
+                    if (args.type === "aws:ec2/vpc:Vpc" ||
+                        args.type === "aws:ec2/subnet:Subnet") {
+                        return {
+                            props: args.props,
+                            opts: pulumi.mergeOptions(args.opts, { ignoreChanges: ["tags"] }),
+                        };
+                    }
+                    return undefined;
+                }],
+            }
+        );
+
+        return vpc;
+    }
+
+    export function awsSecurityGroup
+        (name: string,
+         tags: object,
+         vpcId: Output<string>): aws.ec2.SecurityGroup {
+        // Create the security group. We need a custom security group since we're
+        // going  to expose non-standard ports for things like edge modules. This
+        // is using the pulumi Classic API. Although Crosswalk (awsx) has its own
+        // SecurityGroup type, Cluster wants a Classic SecurityGroup type. See
+        // Cluster.clusterSecurityGroup below where we pass this into the cluster.
+        const sg = new aws.ec2.SecurityGroup(name, {
+            tags: { "Name": name, ...tags },
+            vpcId: vpcId,
+            // TODO I'm not loving this. These should be params I think.
+            ingress: [{
+                description: "Allow inbound traffic on 443",
+                fromPort: 443,
+                toPort: 443,
+                protocol: "tcp",
+                cidrBlocks: ["0.0.0.0/0"]
+            }, {
+                description: "Allow inbound traffic on 9000 for edgeconexion",
+                fromPort: 9000,
+                toPort: 9000,
+                protocol: "tcp",
+                cidrBlocks: ["0.0.0.0/0"]
+            }],
+            egress: [{
+                description: "Allow outbound traffic",
+                fromPort: 0,
+                toPort: 0,
+                protocol: "-1",
+                cidrBlocks: ["0.0.0.0/0"]
+            }]
+        });
+
+        return sg;
+    }
+}
