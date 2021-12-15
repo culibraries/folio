0cfe89d26ac2cb59323d06c1740996c67e3590cb
Steve <steve.ellis@colorado.edu>
2021-12-15 17:59:44 -0500

    Tidying up

--- containers/folio/create-database/create-db.sh
+++ containers/folio/create-database/create-db.sh
@@ -4,11 +4,10 @@
 export PGPASSWORD=$PG_ADMIN_USER_PASSWORD
 
 # Configure Postgres deployed into a kubernetes cluster.
-# TODO Unclear if these two need to run at all. Needs more testing.
 echo "Attempting to create user $DB_USERNAME"
 psql -U $PG_ADMIN_USER -h $DB_HOST -w --command "CREATE USER $DB_USERNAME WITH PASSWORD '"$DB_PASSWORD"';"
 echo "Attempting to create database $DB_DATABASE"
-psql -U $PG_ADMIN_USER -h $DB_HOST -w --command "CREATE DATABASE $DB_DATABASE OWNER $DB_USERNAME"
+psql -U $PG_ADMIN_USER -h $DB_HOST -w --command "CREATE DATABASE $DB_DATABASE OWNER $DB_USERNAME;"
 # This definitely needs to run. See comment in ticket.
 echo "Attempting alter role for $DB_USERNAME"
 psql -U $PG_ADMIN_USER -h $DB_HOST -w --command "ALTER role $DB_USERNAME CREATEROLE CREATEDB SUPERUSER;"
